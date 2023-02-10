decdb23fb71378b5309dc40127096a337f8c17d4
Yanguk <yanguk.heo@knowre.com>
2023-02-10 11:46:28 +0900

    Quest5: 정적화면 구현 완료

--- Quest05/skeleton/notepad.js
+++ Quest05/skeleton/notepad.js
@@ -1,3 +1,30 @@
 class Notepad {
-	/* TODO: 그 외에 또 어떤 클래스와 메소드가 정의되어야 할까요? */
-};
+  /* TODO: 그 외에 또 어떤 클래스와 메소드가 정의되어야 할까요? */
+  wrapperEl;
+
+  constructor(wrapperEl) {
+    this.wrapperEl = wrapperEl;
+    this.template = `
+<section class="explorer">
+    <div class="explorer-header">
+      <p>EXPLORER</p>
+      <button class="new-file-button">새파일 추가</button>
+    </div>
+    <ol class="file-list">
+    </ol>
+</section>
+<section class="main">
+  <div class="tab-wrapper"></div>
+  <div contenteditable="true" class="content-area">...</div>
+</section>
+    `;
+  };
+
+  init() {
+    this.wrapperEl.insertAdjacentHTML('afterbegin', this.template);
+  }
+}
+
+class LocalStorageModel {
+  constructor() {}
+}
